ae4d3e6724730837d88fc3a70ab745c93713e287
Osama AbuSitta <osama.abusitta@gmail.com>
2022-05-14 13:22:50 +0400

    Add project files.

---
+++ Commands/MyCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace InsertGuid
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+
+            var activeDocument = await VS.Documents.GetActiveDocumentViewAsync();
+
+            var textViewSelection = activeDocument.TextView.Selection.SelectedSpans.FirstOrDefault();
+
+            if (textViewSelection != null) 
+            {
+                activeDocument.TextBuffer.Replace(textViewSelection, Guid.NewGuid().ToString());
+            }
+
+        }
+    }
+}
